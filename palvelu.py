5cfdffa38c00b936f040effa04469680e3b15d64
Panu Kalliokoski <atehwa@sange.fi>
2025-02-25 17:22:38 +0200

    poistetaan turha salanimi-muuttuja

--- palvelu.py
+++ palvelu.py
@@ -2,8 +2,6 @@
 def app(environ, respond):
     respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
     nimi = environ['PATH_INFO'].strip('/')
-    salanimi = nimi.replace('a', 'apa').replace('i', 'ipi') \
-            .replace('n', '<b>non</b>').replace('na', 'nana')
     yield "<h1>huomio!</h1>".encode('utf-8')
     yield ("<p><em>nimesi</em> on: %s</p>" % nimi).encode('utf-8')
 
