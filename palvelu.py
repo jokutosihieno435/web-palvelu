38cae7971debcfc68a6dbb56bd2d4ebe4473ed64
Panu Kalliokoski <atehwa@sange.fi>
2025-03-12 19:27:48 +0200

    väliviiva

--- palvelu.py
+++ palvelu.py
@@ -3,5 +3,6 @@ def app(environ, respond):
     respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
     nimi = environ['PATH_INFO'].strip('/')
     yield "<h1>huomio!</h1>".encode('utf-8')
+    yield "<hr size=5>".encode('utf-8')
     yield ("<p><em>nimesi</em> on: %s</p>" % nimi).encode('utf-8')
 
